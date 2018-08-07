55397fcb6a8a3ea8a36ba66c2f392a7ea44717de
Ruben Taelman <rubensworks@gmail.com>
2018-08-07 19:53:38 +0200

    Initial commit

---
+++ src/main/java/org/cyclops/integratedterminalscompat/proxy/ClientProxy.java
@@ -0,0 +1,24 @@
+package org.cyclops.integratedterminalscompat.proxy;
+
+import org.cyclops.cyclopscore.init.ModBase;
+import org.cyclops.cyclopscore.proxy.ClientProxyComponent;
+import org.cyclops.integratedterminalscompat.IntegratedTerminalsCompat;
+
+/**
+ * Proxy for the client side.
+ * 
+ * @author rubensworks
+ * 
+ */
+public class ClientProxy extends ClientProxyComponent {
+
+	public ClientProxy() {
+		super(new CommonProxy());
+	}
+
+	@Override
+	public ModBase getMod() {
+		return IntegratedTerminalsCompat._instance;
+	}
+    
+}
---
+++ src/main/java/org/cyclops/integratedterminalscompat/proxy/CommonProxy.java
@@ -0,0 +1,19 @@
+package org.cyclops.integratedterminalscompat.proxy;
+
+import org.cyclops.cyclopscore.init.ModBase;
+import org.cyclops.cyclopscore.proxy.CommonProxyComponent;
+import org.cyclops.integratedterminalscompat.IntegratedTerminalsCompat;
+
+/**
+ * Proxy for server and client side.
+ * @author rubensworks
+ *
+ */
+public class CommonProxy extends CommonProxyComponent {
+
+    @Override
+    public ModBase getMod() {
+        return IntegratedTerminalsCompat._instance;
+    }
+
+}
