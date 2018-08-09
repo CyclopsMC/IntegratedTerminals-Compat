fd90bee462fc3254499424769b634a99a3d92b90
Ruben Taelman <rubensworks@gmail.com>
2018-08-09 17:57:27 +0200

    Add button to synchronize with JEI search box

--- src/main/java/org/cyclops/integratedterminalscompat/modcompat/jei/JEIIntegratedTerminalsConfig.java
+++ src/main/java/org/cyclops/integratedterminalscompat/modcompat/jei/JEIIntegratedTerminalsConfig.java
@@ -1,13 +1,26 @@
 package org.cyclops.integratedterminalscompat.modcompat.jei;
 
+import mezz.jei.api.IJeiRuntime;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
 import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
+import net.minecraft.client.Minecraft;
+import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.cyclops.cyclopscore.client.gui.component.input.GuiTextFieldExtended;
+import org.cyclops.integratedterminals.api.terminalstorage.ITerminalButton;
+import org.cyclops.integratedterminals.api.terminalstorage.ITerminalStorageTabClient;
+import org.cyclops.integratedterminals.api.terminalstorage.event.TerminalStorageTabClientLoadButtonsEvent;
+import org.cyclops.integratedterminals.api.terminalstorage.event.TerminalStorageTabClientSearchFieldUpdateEvent;
 import org.cyclops.integratedterminals.client.gui.container.GuiTerminalStorage;
+import org.cyclops.integratedterminals.inventory.container.ContainerTerminalStorage;
 import org.cyclops.integratedterminals.part.PartTypes;
 import org.cyclops.integratedterminalscompat.modcompat.jei.terminalstorage.TerminalStorageRecipeTransferHandler;
+import org.cyclops.integratedterminalscompat.modcompat.jei.terminalstorage.button.TerminalButtonItemStackCraftingGridJeiSearchSync;
 
 import javax.annotation.Nonnull;
 
@@ -19,6 +32,8 @@ import javax.annotation.Nonnull;
 @JEIPlugin
 public class JEIIntegratedTerminalsConfig implements IModPlugin {
 
+    private IJeiRuntime jeiRuntime;
+
     @Override
     public void register(@Nonnull IModRegistry registry) {
         if (JEIModCompat.canBeUsed) {
@@ -26,7 +41,52 @@ public class JEIIntegratedTerminalsConfig implements IModPlugin {
             registry.addRecipeClickArea(GuiTerminalStorage.class, 86, 76, 22, 15, VanillaRecipeCategoryUid.CRAFTING);
             registry.getRecipeTransferRegistry().addUniversalRecipeTransferHandler(new TerminalStorageRecipeTransferHandler(registry.getJeiHelpers().recipeTransferHandlerHelper()));
             registry.addRecipeCatalyst(new ItemStack(PartTypes.TERMINAL_STORAGE.getItem()), VanillaRecipeCategoryUid.CRAFTING);
+
+            MinecraftForge.EVENT_BUS.register(this);
         }
     }
 
+    @Override
+    public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
+        this.jeiRuntime = jeiRuntime;
+    }
+
+    @SubscribeEvent
+    public void onTerminalStorageButtons(TerminalStorageTabClientLoadButtonsEvent event) {
+        event.getButtons().add(new TerminalButtonItemStackCraftingGridJeiSearchSync());
+    }
+
+    protected boolean isSearchSynced(ITerminalStorageTabClient<?> clientTab) {
+        for (ITerminalButton<?, ?, ?> button : clientTab.getButtons()) {
+            if (button instanceof TerminalButtonItemStackCraftingGridJeiSearchSync) {
+                return ((TerminalButtonItemStackCraftingGridJeiSearchSync) button).isActive();
+            }
+        }
+        return false;
+    }
+
+    @SubscribeEvent
+    public void onSearchFieldUpdated(TerminalStorageTabClientSearchFieldUpdateEvent event) {
+        // Copy the terminal search box contents into the JEI search box.
+        if (isSearchSynced(event.getClientTab())) {
+            jeiRuntime.getIngredientFilter().setFilterText(event.getSearchString() + "");
+        }
+    }
+
+    @SubscribeEvent
+    public void onKeyTyped(GuiScreenEvent.KeyboardInputEvent.Post event) {
+        // Copy the JEI search box contents into the terminal search box.
+        if (event.getGui() instanceof GuiTerminalStorage) {
+            GuiTerminalStorage gui = ((GuiTerminalStorage) event.getGui());
+            if (jeiRuntime.getIngredientListOverlay().hasKeyboardFocus()) {
+                GuiTextFieldExtended fieldSearch = gui.getFieldSearch();
+                fieldSearch.setText(jeiRuntime.getIngredientFilter().getFilterText());
+                gui.getSelectedClientTab().ifPresent(tab -> {
+                    if (isSearchSynced(tab)) {
+                        tab.setInstanceFilter(gui.getContainer().getSelectedChannel(), fieldSearch.getText() + "");
+                    }
+                });
+            }
+        }
+    }
 }
---
+++ src/main/java/org/cyclops/integratedterminalscompat/modcompat/jei/terminalstorage/button/TerminalButtonItemStackCraftingGridJeiSearchSync.java
@@ -0,0 +1,56 @@
+package org.cyclops.integratedterminalscompat.modcompat.jei.terminalstorage.button;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.cyclops.cyclopscore.client.gui.component.button.GuiButtonImage;
+import org.cyclops.cyclopscore.helper.L10NHelpers;
+import org.cyclops.integratedterminals.api.terminalstorage.ITerminalButton;
+import org.cyclops.integratedterminals.client.gui.image.Images;
+import org.cyclops.integratedterminals.core.terminalstorage.TerminalStorageTabIngredientComponentClient;
+import org.cyclops.integratedterminals.core.terminalstorage.TerminalStorageTabIngredientComponentCommon;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+/**
+ * A button for toggling JEI search box sync.
+ * @author rubensworks
+ */
+public class TerminalButtonItemStackCraftingGridJeiSearchSync
+        implements ITerminalButton<TerminalStorageTabIngredientComponentClient<?, ?>,
+        TerminalStorageTabIngredientComponentCommon<?, ?>, GuiButtonImage> {
+
+    private boolean active = false;
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public GuiButtonImage createButton(int x, int y) {
+        return new GuiButtonImage(0, x, y,
+                active ? Images.BUTTON_BACKGROUND_ACTIVE : Images.BUTTON_BACKGROUND_INACTIVE,
+                Images.BUTTON_MIDDLE_JEI_SYNC);
+    }
+
+    @Override
+    public void onClick(TerminalStorageTabIngredientComponentClient<?, ?> clientTab, @Nullable TerminalStorageTabIngredientComponentCommon<?, ?> commonTab, GuiButtonImage guiButton, int channel, int mouseButton) {
+        this.active = !this.active;
+    }
+
+    @Override
+    public String getUnlocalizedName() {
+        return "gui.integratedterminalscompat.terminal_storage.craftinggrid.jeisync";
+    }
+
+    @Override
+    public void getTooltip(EntityPlayer player, ITooltipFlag tooltipFlag, List<String> lines) {
+        lines.add(L10NHelpers.localize("gui.integratedterminalscompat.terminal_storage.craftinggrid.jeisync.info"));
+        lines.add(TextFormatting.ITALIC + L10NHelpers.localize(
+                active ? "general.cyclopscore.info.enabled" : "general.cyclopscore.info.disabled"));
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+}
