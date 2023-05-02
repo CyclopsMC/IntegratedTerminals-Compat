234cf3147298ee1ffc169b5ce4271950d2c17e6f
Simon Gerstmeier <simon@gerstmeier.dev>
2023-05-02 19:34:26 +0200

    Replaced deprecated JEI API functions IGuiContainerHandler#getIngredientUnderMouse and IIngredientManager#getIngredientType

--- src/main/java/org/cyclops/integratedterminalscompat/modcompat/jei/terminalstorage/TerminalStorageGuiHandler.java
+++ src/main/java/org/cyclops/integratedterminalscompat/modcompat/jei/terminalstorage/TerminalStorageGuiHandler.java
@@ -1,39 +1,56 @@
 package org.cyclops.integratedterminalscompat.modcompat.jei.terminalstorage;
 
 import mezz.jei.api.gui.handlers.IGuiContainerHandler;
-import org.cyclops.integratedterminals.api.terminalstorage.ITerminalStorageTabClient;
+import mezz.jei.api.ingredients.IIngredientType;
+import mezz.jei.api.ingredients.ITypedIngredient;
+import mezz.jei.api.runtime.IClickableIngredient;
+import mezz.jei.api.runtime.IIngredientManager;
+import mezz.jei.common.input.ClickableIngredient;
+import mezz.jei.common.util.ImmutableRect2i;
 import org.cyclops.integratedterminals.client.gui.container.ContainerScreenTerminalStorage;
 import org.cyclops.integratedterminals.core.terminalstorage.TerminalStorageTabIngredientComponentClient;
 import org.cyclops.integratedterminalscompat.modcompat.jei.JEIIntegratedTerminalsConfig;
+import org.jetbrains.annotations.NotNull;
 
-import javax.annotation.Nullable;
 import java.util.Optional;
 
 /**
  * This handler allows JEI to recognise the terminal storage slot contents.
  * @author rubensworks
+ * @author Snonky
  */
 public class TerminalStorageGuiHandler implements IGuiContainerHandler<ContainerScreenTerminalStorage<?, ?>> {
-    @Nullable
+
     @Override
-    public Object getIngredientUnderMouse(ContainerScreenTerminalStorage<?, ?> guiContainer, double mouseX, double mouseY) {
-        int slotIndex = guiContainer.getStorageSlotIndexAtPosition(mouseX, mouseY);
-        if (slotIndex >= 0) {
-            Optional<ITerminalStorageTabClient<?>> tabOptional = guiContainer.getSelectedClientTab();
-            if (tabOptional.isPresent()) {
-                ITerminalStorageTabClient<?> tab = tabOptional.get();
-                if (tab instanceof TerminalStorageTabIngredientComponentClient) {
-                    Object instance = ((TerminalStorageTabIngredientComponentClient) tab).getSlotInstance(
-                            guiContainer.getMenu().getSelectedChannel(), slotIndex).orElse(null);
-                    try {
-                        JEIIntegratedTerminalsConfig.jeiRuntime.getIngredientManager().getIngredientType(instance);
-                        return instance;
-                    } catch (IllegalArgumentException | NullPointerException e) {
-                        // Ignore ingredient types unsupported by JEI
-                    }
-                }
+    public @NotNull Optional<IClickableIngredient<?>> getClickableIngredientUnderMouse(
+            @NotNull ContainerScreenTerminalStorage<?, ?> containerScreen, double mouseX, double mouseY) {
+        Optional<? extends IClickableIngredient<?>> clickableIngredientOptional =
+                createClickableIngredient(containerScreen, mouseX,  mouseY);
+        return clickableIngredientOptional.map((clickableIngredient) -> (IClickableIngredient<?>) clickableIngredient);
+    }
+
+    private <T> Optional<IClickableIngredient<T>> createClickableIngredient(
+            ContainerScreenTerminalStorage<T, ?> containerScreen, double mouseX, double mouseY) {
+        int slotIndex = containerScreen.getStorageSlotIndexAtPosition(mouseX, mouseY);
+        @SuppressWarnings("unchecked") // Cast is safe due to filter
+        Optional<TerminalStorageTabIngredientComponentClient<T, ?>> tabOptional = containerScreen.getSelectedClientTab()
+                .filter(TerminalStorageTabIngredientComponentClient.class::isInstance)
+                .map(TerminalStorageTabIngredientComponentClient.class::cast);
+        if(slotIndex >= 0 && tabOptional.isPresent()) {
+            TerminalStorageTabIngredientComponentClient<T, ?> tab = tabOptional.get();
+            int channel = containerScreen.getMenu().getSelectedChannel();
+            IIngredientManager ingredientManager = JEIIntegratedTerminalsConfig.jeiRuntime.getIngredientManager();
+            Optional<T> instanceOptional = tab.getSlotInstance(channel, slotIndex);
+            Optional<IIngredientType<T>> ingredientTypeOptional = instanceOptional
+                    .flatMap(ingredientManager::getIngredientTypeChecked);
+            if (instanceOptional.isPresent() && ingredientTypeOptional.isPresent()) {
+                ImmutableRect2i slotRect = new ImmutableRect2i(containerScreen.getStorageSlotRect(slotIndex));
+                Optional<ITypedIngredient<T>> typedIngredientOptional = ingredientManager
+                        .createTypedIngredient(ingredientTypeOptional.get(), instanceOptional.get());
+                return typedIngredientOptional
+                        .map((typedIngredient) -> new ClickableIngredient<>(typedIngredient, slotRect));
             }
         }
-        return null;
+        return Optional.empty();
     }
 }
