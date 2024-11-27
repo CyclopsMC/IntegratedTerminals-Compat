c671d30254837952009294900b5b33066ec21b76
Ruben Taelman <rubensworks@gmail.com>
2024-11-27 09:26:59 +0100

    Fix JEI lag caused by non-crafting recipes being considered in terminal
    Closes CyclopsMC/IntegratedTerminals#141

--- src/main/java/org/cyclops/integratedterminalscompat/modcompat/jei/JEIIntegratedTerminalsConfig.java
+++ src/main/java/org/cyclops/integratedterminalscompat/modcompat/jei/JEIIntegratedTerminalsConfig.java
@@ -70,10 +70,10 @@ public class JEIIntegratedTerminalsConfig implements IModPlugin {
 
     @Override
     public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
-        registration.addUniversalRecipeTransferHandler(
-                new TerminalStorageRecipeTransferHandler<>(ContainerTerminalStoragePart.class, RegistryEntries.CONTAINER_PART_TERMINAL_STORAGE_PART));
-        registration.addUniversalRecipeTransferHandler(
-                new TerminalStorageRecipeTransferHandler<>(ContainerTerminalStorageItem.class, RegistryEntries.CONTAINER_PART_TERMINAL_STORAGE_ITEM));
+        registration.addRecipeTransferHandler(
+                new TerminalStorageRecipeTransferHandler<>(ContainerTerminalStoragePart.class, RegistryEntries.CONTAINER_PART_TERMINAL_STORAGE_PART), RecipeTypes.CRAFTING);
+        registration.addRecipeTransferHandler(
+                new TerminalStorageRecipeTransferHandler<>(ContainerTerminalStorageItem.class, RegistryEntries.CONTAINER_PART_TERMINAL_STORAGE_ITEM), RecipeTypes.CRAFTING);
     }
 
     @Override
