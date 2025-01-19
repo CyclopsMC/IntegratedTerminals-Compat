0c27cdfe128e65e97d2cc4d0ecf27cc1aec39ae7
Ruben Taelman <rubensworks@gmail.com>
2025-01-19 09:56:22 +0100

    Fix EMI render issue when showing button-less recipes
    Closes CyclopsMC/IntegratedTerminals#150

--- src/main/java/org/cyclops/integratedterminalscompat/modcompat/emi/terminalstorage/TerminalStorageEmiRecipeHandler.java
+++ src/main/java/org/cyclops/integratedterminalscompat/modcompat/emi/terminalstorage/TerminalStorageEmiRecipeHandler.java
@@ -155,8 +155,10 @@ public class TerminalStorageEmiRecipeHandler<T extends ContainerTerminalStorageB
                         }
 
                         // Render overlay on button
-                        Bounds bounds = fillButton.get().getBounds();
-                        GuiComponent.fill(matrices, bounds.x(), bounds.y(), bounds.x() + bounds.width(), bounds.y() + bounds.height(), transferResult.getButtonHighlightColor());
+                        if (fillButton.get() != null) {
+                            Bounds bounds = fillButton.get().getBounds();
+                            GuiComponent.fill(matrices, bounds.x(), bounds.y(), bounds.x() + bounds.width(), bounds.y() + bounds.height(), transferResult.getButtonHighlightColor());
+                        }
                     });
                 });
     }
